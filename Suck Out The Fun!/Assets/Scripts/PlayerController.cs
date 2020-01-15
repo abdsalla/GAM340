7babcd433cf65f3a7d916310cc637337bf22210b
Duli <35743457+abdsalla@users.noreply.github.com>
2020-01-14 17:23:38 -0700

    Bleend tree added, Pawn and Controller

---
+++ Suck Out The Fun!/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Pawn pawn;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
