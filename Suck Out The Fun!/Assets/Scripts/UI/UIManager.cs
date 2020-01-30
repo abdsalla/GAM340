df434b4c0907143e3bc16e5e0318f68ff44ade3b
Duli <abdsalla@uat.edu>
2020-01-30 16:53:18 -0700

    GameManager and UIManager added

---
+++ Suck Out The Fun!/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UIManager : MonoBehaviour
+{
+    public Health playerHealthComponent;
+
+    [Header("UI Elements")]
+    public Image healthBar;
+
+    private void Start()
+    {
+        playerHealthComponent.OnHealthChange.AddListener(UpdateHealthUI);
+    }
+
+    public void UpdateHealthUI (float newhealthvalue)
+    {
+        Debug.Log("Health has been changed to " + newhealthvalue);
+        healthBar.fillAmount = newhealthvalue;
+    }
+
+    public float CalculateHealth()
+    {
+        return playerHealthComponent.CurrentHealth / playerHealthComponent.maxHealth;
+    }
+}
